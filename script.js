5bba0c1b41ea488a0a0223c6fae436a393a42e2f
papulosophy <hiznibzdapope@gmail.com>
2023-08-04 12:11:05 +0200

    exo tableau 1 et 2

---
+++ script.js
@@ -0,0 +1,52 @@
+// Exo 1
+
+let myAlphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "$",
+  "*",
+  "/",
+  "-",
+  "+",
+];
+
+console.log(myAlphabet.length); // Résultat 31
+
+function myAlphabetLength() {
+  console.log(myAlphabet.length);
+}
+
+myAlphabetLength();
+
+// Exo 2
+
+let planets = ["terre", "venus", "mars", "mercure", "saturne"];
+console.log(terre);
+console.log(venus);
+console.log(mars);
+console.log(mercure);
+console.log(saturne);
